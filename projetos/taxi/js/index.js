4740c6dfff0702812f44cd0fc55261712c9fabd5
Pedro Coelho <pedroncoelhon@gmail.com>
2025-02-12 09:17:52 -0300

    all update in taxi

--- projetos/taxi/js/index.js
+++ projetos/taxi/js/index.js
@@ -1,12 +1,21 @@
-function que() {
-    let total1 = document.getElementById("total").value;
-    let total2 = document.getElementById("total2").value;
-    let coisado1 = document.getElementById("coisado").value;
-    let combustivel = 6.50
+function que(){
+    let total1 = parseInt(document.getElementById("total").value),
+        total2 = parseInt(document.getElementById("total2").value),
+        coisado1 = parseFloat(document.getElementById("coisado").value),
+        coisado2 = parseFloat(document.getElementById("coisado2").value);
+    let combustivel = 6.50;
 
-    let km = total1 + total2 / 1000              
-    let gasto = km / combustivel
-    let lucro = coisado1 / gasto
+    let metros = total2 - total1;
+    let gastosL =  metros / coisado1;
+    let preju = gastosL * combustivel;
+    let lucro = coisado2 - preju;
+    
+    km = metros / 1000;
 
-    document.getElementById("resultado").innerHTML = "carro andou por: " + km + "km \n combustivel gasto foi de:" + gasto + "Litros. \n  o valor do combustivel hoje é R$" + combustivel + ". \n o valor total ganho pela corrida foi: R$" + coisado1 + "\n lucro de: R$" + lucro
+    let texto = ("carro andou por: " + km + "km \n" +
+    "<br> <br> gastou de combustivel em: " + gastosL + " Litros. \n" + 
+    "<br> <br> o valor do combustivel hoje é R$ " + combustivel +  
+    "<br> <br> lucro de: R$" + lucro);
+
+    document.getElementById("resultado").innerHTML = texto;
 }
