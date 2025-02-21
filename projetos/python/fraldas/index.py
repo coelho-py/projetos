7d88fbc160f66d73c4824b12a4d86366c92852d8
coelho-py <pedroncoelhon@gmail.com>
2025-02-21 09:36:23 -0300

    21 02

---
+++ projetos/python/fraldas/index.py
@@ -0,0 +1,4 @@
+dias = float(input("quantas fraldas ira usar por dia?"))
+valor = 1.1
+total = dias * valor * 30
+print(f'ira usar: {dias} fraldas no valor de {valor} gastando total de {total}')
---
+++ projetos/python/login/index.py
@@ -0,0 +1,18 @@
+escolha = print(f'Tela Inicial \n\n 1 - registrar \n\ 2 - login')
+#if escolha == 1:
+#    return registro()
+
+class registro:
+    def _int_(escolha):
+        nome = input("qual ẽ seu nome? \n")
+        cidade = input("escolha cidade por numero \n 1 - São joão dos Patos \n 2 - Asa Dourada \n 3 - Lago do Pato Feio")
+
+        if cidade == 1:
+            print(f'cidade sao jao')
+        elif cidade == 2:
+            print(f'asa dourada')
+
+#filhos = int(input("numero de filhos: \n"))
+#plano = input("plano de saude? \n 1 - sim \n 2 - nao")
+#vale = input("vale de transporte? \n 1 - sim \n 2 - nao")
+#cargo = input("qual seu cargo? \n 1 - Engenheiro  \n 2 - Mestre de Obra \n 3 - Pedreiro")
