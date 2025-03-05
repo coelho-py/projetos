28855ed63478c5e6562998dbe63608efc4e66d35
Pedro Coelho <pedroncoelhon@gmail.com>
2025-03-05 15:55:18 -0300

    finalmente arrumei

--- projetos/python/cardapio/index.py
+++ projetos/python/cardapio/index.py
@@ -7,30 +7,53 @@ print("Hamburguer  104  1,20")
 print("cheeseburguer  105  1,30")
 print("refrigerante  106  1,00")
 
-codigo = float(input("Digite o codigo do produto\n"))
-if (not codigo or codigo > 107 or codigo < 101):
-    print("codigo invalido")
-else:
-    quantidade = float(input("Digita a quantidade\n"))
+repeticao = 0
+total_geral = 0
+itens = []
 
-if(codigo == 101):
-    produto = 1.20
-    nome = "cachorro quente"
-elif(codigo == 102):
-    produto = 1.30
-    nome = "bauru simples"
-elif(codigo == 103):
-    produto = 1.50
-    nome = "bauru com ovo"
-elif(codigo == 104):
-    produto = 1.20
-    nome = "hamburguer"
-elif(codigo == 105):
-    produto = 1.30
-    nome = "cheeseburguer"
-elif(codigo == 106):
-    produto = 1
-    nome = "refrigerante"
+while(repeticao <= 99):
+    codigo = int(input("Digite o codigo do produto\n"))
+    
+    if (codigo not in [101, 102, 103, 104, 105, 106]):
+        print("codigo invalido")
+        break
+    else:
+        quantidade = int(input("Digite a quantidade\n"))
+    
+    if(codigo == 101):
+        produto = 1.20
+        nome = "cachorro quente"
+    elif(codigo == 102):
+        produto = 1.30
+        nome = "bauru simples"
+    elif(codigo == 103):
+        produto = 1.50
+        nome = "bauru com ovo"
+    elif(codigo == 104):
+        produto = 1.20
+        nome = "hamburguer"
+    elif(codigo == 105):
+        produto = 1.30
+        nome = "cheeseburguer"
+    elif(codigo == 106):
+        produto = 1.00
+        nome = "refrigerante"
+    
+    total = produto * quantidade
+    total_geral += total
+    itens.append(f"{quantidade} x {nome} = {total:.2f}")
+    
+    print(f"{quantidade} x {nome} = {total:.2f}")
+    
+    escolha = input("Deseja continuar? (Digite 1 para continuar, 2 para sair)\n")
+    
+    if escolha == '2':
+        break
+    
+    repeticao += 1
 
-valor = produto * quantidade
-print(f'voce escolheu o produto:' + str(nome) + '\nna quantidade de: ' + str(quantidade) + '\ndo valor de:' + str(produto) + '\nvalor total: ' + str(valor))
+print("\n=== Resumo do Pedido ===")
+for item in itens:
+    print(item)
+
+print(f"\nTotal geral: R$ {total_geral:.2f}")
